b0829eee9f0a4717c034ef01d23d2f4728547df4
Shanea King-Roberson <kingroberson@kingroberson-macbookpro.local>
2016-09-01 19:23:31 -0700

    Initial Commit

---
+++ UsConnected/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UsConnected
+//
+//  Created by Shanea King-Roberson on 9/1/16.
+//  Copyright © 2016 Shanea King-Roberson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
