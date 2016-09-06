fd930b91bca4111b06ddb3090324420826bbafc6
Shanea King-Roberson <kingroberson@kingroberson-macbookpro.fios-router.home>
2016-09-06 12:47:20 -0400

    First commit with bugs :(

--- UsConnected/ViewController.swift
+++ UsConnected/ViewController.swift
@@ -7,9 +7,37 @@
 //
 
 import UIKit
+import Firebase
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var table: UITableView!
+    
+    internal func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+         return 10
+    }
+    
+   
+    internal func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        
+        
+        
+    }
+
+    let firebaseRef = FIRDatabase().reference
+    let dataSource : FirebaseTableViewDataSource!
+    
+    self.dataSource = FirebaseTableViewDataSource(ref: self.firebaseRef, cellReuseIdentifier: "Cell", view: self.tableView)
+    
+    self.dataSource.populateCellWithBlock { (cell: UITableViewCell, obj: NSObject) -> Void in
+      let snap = obj as! FIRDataSnapshot
+    
+      // Populate cell as you see fit, like as below
+      cell.textLabel?.text = snap.key as String
+    }
+    
+    self.tableView.dataSource = self.dataSource
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
